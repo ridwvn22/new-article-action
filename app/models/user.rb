2c73301ebc6565f1836bba55b7f315b2aa33608c
Ridwan Dahir <ridwandahir@Ridwans-MacBook-Air-2.local>
2023-11-22 09:29:04 -0600

    solution to blog app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
-  has_one_attached :avatar
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :validatable
+
+validates :first_name, :last_name, :username, presence: true
 end
